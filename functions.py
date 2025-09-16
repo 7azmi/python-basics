84d34d4c76ea73b3beb006793ad4370859797b72
Ahmed Ghaleb <58638184+7azmi@users.noreply.github.com>
2025-09-16 16:35:09 +0800

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,18 @@
+# Week 1 Task 3 Part 1: Functions
+# [YOURNAME]
+
+# Video link: https://youtu.be/0ykzaQ4QFNU
+
+# In the video, I explained an example of why to use a function in your code
+# Now, after we've watched the video and understood why you need the function, 
+# you need to do the following:
+
+# TODO: Print 10 users' **name** and  **age**, and print if the user is **adult** or **underage**
+# You can make up some random info, or friends. It does not matter. (Main Task - 3 tokens)
+
+
+# TODO: Can you print all 10 users without repeating the function you created? (Subtask - 2 tokens)
+# hint: `list` & `loop` 
+
+# NOTE: The whole point of this task is to NOT repeat code (DRY). 
+# You'll get evaluated based on that. Good luck😼
